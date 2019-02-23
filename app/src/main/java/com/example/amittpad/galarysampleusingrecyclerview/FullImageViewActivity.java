1307c8596e5f7debc12f4fc707b08e392d392fd0
Agniva <agniva24@gmail.com>
2019-02-23 11:10:09 +0530

    To added full image view in activity with cross button

--- app/src/main/java/com/example/amittpad/galarysampleusingrecyclerview/FullImageViewActivity.java
+++ app/src/main/java/com/example/amittpad/galarysampleusingrecyclerview/FullImageViewActivity.java
@@ -2,7 +2,9 @@ package com.example.amittpad.galarysampleusingrecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.koushikdutta.ion.Ion;
 
@@ -13,23 +15,41 @@ import uk.co.senab.photoview.PhotoView;
 public class FullImageViewActivity extends AppCompatActivity {
     private String imagePreView;
     public ArrayList<ImageListPojo> myArrayList = new ArrayList<>();
+    private ImageView btnCancel;
+    private PhotoView photoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        PhotoView photoView=new PhotoView(this);
-        photoView.setMaximumScale(16f);
-        setContentView(photoView);
+        /** To show full image with out any activity **/
+//        PhotoView photoView = new PhotoView(this);
+//        photoView.setMaximumScale(16f);
+//        setContentView(photoView);
+//
+//        imagePreView = getIntent().getExtras().getString("Position_Id");
+//
+//        //Loading Image View using Picasso with PhotoView
+//       /* Picasso.with(this)
+//                .load(imagePreView)
+//                .resize(240, 120)
+//                .centerCrop()
+//                .into(photoView);*/
+//
+//        //Loading Image View using Picasso with PhotoView
+//        Ion.with(photoView)
+//                .placeholder(R.drawable.placeholder)
+//                .error(R.drawable.error_pic)
+//                .animateIn(AnimationUtils.loadAnimation(this, R.anim.fade_in_animation))
+//                .load(imagePreView);
 
-      imagePreView = getIntent().getExtras().getString("Position_Id");
 
-        //Loading Image View using Picasso with PhotoView
-       /* Picasso.with(this)
-                .load(imagePreView)
-                .resize(240, 120)
-                .centerCrop()
-                .into(photoView);*/
+        /** To show full image using activity with cancel button **/
+        setContentView(R.layout.activity_full_image_view);
+        imagePreView = getIntent().getExtras().getString("Position_Id");
+        btnCancel = (ImageView) findViewById(R.id.btn_cancel);
+        photoView=(PhotoView)findViewById(R.id.photo_view);
+        photoView.setMaximumScale(10f);
 
         //Loading Image View using Picasso with PhotoView
         Ion.with(photoView)
@@ -39,6 +59,11 @@ public class FullImageViewActivity extends AppCompatActivity {
                 .load(imagePreView);
 
 
-
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
